1f6d9e23d577d0db54ec7be5e601f1cd786df17f
Daniel Torres <danstorre@gmail.com>
2021-07-11 22:57:58 -0300

    service loads data using the whisper id passed into its load function.

--- WhisperApp/RemoteReplyThreadLoaderAPI/RemoteReplyThreadLoader.swift
+++ WhisperApp/RemoteReplyThreadLoaderAPI/RemoteReplyThreadLoader.swift
@@ -9,4 +9,15 @@ public class RemoteReplyThreadLoader {
         self.url = url
         self.client = client
     }
+    
+    public func load(from id: String) {
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        let queryItem = URLQueryItem(name: "wid", value: id)
+        
+        urlComponents?.queryItems = [queryItem]
+        
+        if let serviceURL = urlComponents?.url {
+            client.getDataFrom(url: serviceURL)
+        }
+    }
 }
--- WhisperAppTests/RemoteReplyThreadLoaderTests.swift
+++ WhisperAppTests/RemoteReplyThreadLoaderTests.swift
@@ -12,6 +12,18 @@ class RemoteReplyThreadLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_serviceLoadsDataUsingAnWhisperID() {
+        let url = URL(string: "http://a-url.com")!
+        let whisperId = "anUUID"
+        let client = HTTPClientSpy()
+        let sut = RemoteReplyThreadLoader(url: url, client: client)
+        
+        sut.load(from: whisperId)
+        
+        XCTAssertEqual(client.requestedURL?.absoluteString,
+                       "http://a-url.com?wid=\(whisperId)")
+    }
+    
     // MARK:- Helpers
     
     private class HTTPClientSpy: HTTPClient {
