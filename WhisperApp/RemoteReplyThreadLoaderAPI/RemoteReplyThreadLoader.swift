4c751f7d5cc19b5f1c1142b3890fc3600cc6eaaf
Daniel Torres <danstorre@gmail.com>
2021-07-13 17:21:08 -0300

    move `RemoteWhisperReply` to its own file.

--- WhisperApp/RemoteReplyThreadLoaderAPI/RemoteReplyThreadLoader.swift
+++ WhisperApp/RemoteReplyThreadLoaderAPI/RemoteReplyThreadLoader.swift
@@ -1,30 +1,6 @@
 
 import Foundation
 
-public struct RemoteWhisperReply: Equatable, Decodable {
-    public let id: String
-    public let heartCount: Int
-    public let replies: Int
-    public let text: String
-    public let imageURL: URL
-    
-    enum CodingKeys: String, CodingKey {
-        case id = "wid"
-        case heartCount = "me2"
-        case replies
-        case text
-        case imageURL = "url"
-    }
-    
-    public init(id: String, heartCount: Int, replies: Int, text: String, imageURL: URL) {
-        self.id = id
-        self.heartCount = heartCount
-        self.replies = replies
-        self.text = text
-        self.imageURL = imageURL
-    }
-}
-
 public final class RemoteReplyThreadLoader {
     private let url: URL
     private let client: HTTPClient
