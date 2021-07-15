07ceede20f18e3f856c51fc29d9e4e00f03565d2
Daniel Torres <danstorre@gmail.com>
2021-07-15 13:48:41 -0300

    create  view

---
+++ PopularWhispers/PopularWhispers/UI/SwiftUI/PopularThreadFromWhisper.swift
@@ -0,0 +1,56 @@
+
+import SwiftUI
+
+struct WhisperPresentableData: Hashable {
+    let description: String
+    let heartCount: String
+    let image: UIImage?
+}
+
+struct PopularThreadFromWhisper: View {
+    let title: String
+    let whispersPresentable: [WhisperPresentableData]
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+            VStack {
+                ForEach(whispersPresentable, id:\.self) { whisper in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Heart count: \(whisper.heartCount)")
+                            Text(whisper.description)
+                        }
+                        Spacer()
+                    }
+                    .border(Color.blue, width: 2)
+                    .padding()
+                }
+                Spacer()
+            }.padding()
+        }
+    }
+}
+
+struct PopularThreadFromWhisper_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            let whisper1 = WhisperPresentableData(description: "a text 1", heartCount: "2", image: nil)
+            let whisper2 = WhisperPresentableData(description: "a text 2", heartCount: "4", image: nil)
+            let whisper3 = WhisperPresentableData(description:
+"""
+a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large a very large text
+""", heartCount: "4", image: nil)
+            let whispers = [whisper1, whisper2, whisper3]
+            
+            PopularThreadFromWhisper(title: "Most Popular Thread", whispersPresentable: whispers)
+            
+            PopularThreadFromWhisper(title: "Most Popular Thread", whispersPresentable: whispers)
+                .preferredColorScheme(.dark)
+        }
+        
+    }
+}
