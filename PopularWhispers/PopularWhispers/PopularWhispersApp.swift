9218dcdfcdda6ee39e514533ceb407801b4d0d9a
Daniel Torres <danstorre@gmail.com>
2021-07-15 11:34:58 -0300

    created PopularWhisperApp workspace.

---
+++ PopularWhispers/PopularWhispers/PopularWhispersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularWhispersApp.swift
+//  PopularWhispers
+//
+//  Created by Daniel Torres on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct PopularWhispersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
