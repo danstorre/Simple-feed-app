fb09de62acdd2b4d0c4d0b5160887f0e21b03610
Daniel Torres <danstorre@gmail.com>
2021-07-16 12:36:02 -0300

    compose views into a navigation view.

--- PopularWhispers/PopularWhispers/PopularWhispersApp.swift
+++ PopularWhispers/PopularWhispers/PopularWhispersApp.swift
@@ -6,7 +6,25 @@ import SwiftUI
 struct PopularWhispersApp: App {
     var body: some Scene {
         WindowGroup {
-            FactoryPopularWhisperFeedView.create()
+            ContentView()
         }
+    }
+}
+
+struct ContentView: View {
+    @State var nextView: WhisperListView!
+    @State var pushed: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                FactoryPopularWhisperFeedView.create { whisperListView in
+                    nextView = whisperListView
+                    pushed = true
+                }
+                
+                NavigationLink(destination: nextView, isActive: $pushed) {}
+            }
+        }.navigationTitle("Popular")
     }
 }
--- PopularWhispers/PopularWhispers/UI/Main/Factories/FactoryPopularWhispersView.swift
+++ PopularWhispers/PopularWhispers/UI/Main/Factories/FactoryPopularWhispersView.swift
@@ -1,5 +1,7 @@
 
 import Foundation
+import WhisperApp
+import SwiftUI
 
 enum FactoryPopularWhisperFeedView {
     static func create() -> WhisperListView {
@@ -8,5 +10,40 @@ enum FactoryPopularWhisperFeedView {
         
         return WhisperListView(title: "Popular Whispers",
                                viewModel: whisperListAdapter)
+    }
+    
+    static func create(selection: @escaping (WhisperListView) -> Void ) -> WhisperListView {
+        let popularRepliesVM: PopularWhisperFeedVM = PopularWhisperFeedVM(loader: FactoryPopularWhisperFeedLoader.create())
+        let coordinator = WhisperCoordinator(loader: FactoryPopularWhisperFeedLoader.create())
+        
+        let whisperListAdapter = AdapterWhisperList(loader: popularRepliesVM, handler: { selectedId in
+            coordinator.navigateToDetailsFrom(id: selectedId,
+                                              completion: selection)
+        })
+        
+        return WhisperListView(title: "Popular Whispers",
+                               viewModel: whisperListAdapter)
+    }
+}
+
+class WhisperCoordinator {
+    private let loader: PopularFeedLoader
+    
+    init(loader: PopularFeedLoader) {
+        self.loader = loader
+    }
+    
+    func navigateToDetailsFrom(id: String, completion: @escaping (WhisperListView) -> Void ) {
+        loader.loadPopularFeedLoader { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success(whispers):
+                guard let selectedWhisper = whispers.first(where: { $0.wildCardID == id }) else { return }
+                completion(FactoryPopularThreadView.create(from: selectedWhisper))
+                
+            case .failure(_): break
+            }
+        }
     }
 }
