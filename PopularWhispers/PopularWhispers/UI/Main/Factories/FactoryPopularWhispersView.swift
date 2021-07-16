853ba4be87f5b8947d188d1ed15d001bde52bfeb
Daniel Torres <danstorre@gmail.com>
2021-07-16 12:37:25 -0300

    add `WhisperCoordinator` into its own file.

--- PopularWhispers/PopularWhispers/UI/Main/Factories/FactoryPopularWhispersView.swift
+++ PopularWhispers/PopularWhispers/UI/Main/Factories/FactoryPopularWhispersView.swift
@@ -25,25 +25,3 @@ enum FactoryPopularWhisperFeedView {
                                viewModel: whisperListAdapter)
     }
 }
-
-class WhisperCoordinator {
-    private let loader: PopularFeedLoader
-    
-    init(loader: PopularFeedLoader) {
-        self.loader = loader
-    }
-    
-    func navigateToDetailsFrom(id: String, completion: @escaping (WhisperListView) -> Void ) {
-        loader.loadPopularFeedLoader { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success(whispers):
-                guard let selectedWhisper = whispers.first(where: { $0.wildCardID == id }) else { return }
-                completion(FactoryPopularThreadView.create(from: selectedWhisper))
-                
-            case .failure(_): break
-            }
-        }
-    }
-}
